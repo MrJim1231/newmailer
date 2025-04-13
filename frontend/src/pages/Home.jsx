52e10bbce188203b8ce4c88f39ee538ba53fb78c
Oleh <berolegnik@gmail.com>
2025-04-13 13:30:51 +0300

    faq

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import Question from '../components/faq/Question' // Импортируем компонент QuestionOne
+import Question from '../components/faq/Question' // Импортируем компонент Question
+import QuestionTwo from '../components/faq/QuestionTwo' // Импортируем компонент QuestionTwo
 import styles from '../styles/Home.module.css'
 
 const Home = () => {
   return (
     <div className={styles.container}>
-      <h4>Как сделать так, чтобы письмо шло во «Входящие», а не в СПАМ!</h4>
-
-      {/* Вставляем компонент QuestionOne */}
+      {/* Вставляем оба компонента Question и QuestionTwo */}
       <Question />
+      <QuestionTwo />
     </div>
   )
 }
